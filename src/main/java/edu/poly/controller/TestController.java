38f8d874b24f32cf728d94465fee915f42f88c33
tientrananhhung <tien.trananhhung@gmail.com>
2018-09-25 21:36:33 +0700

    Initial commit

---
+++ src/main/java/edu/poly/controller/TestController.java
@@ -0,0 +1,16 @@
+package edu.poly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+    @GetMapping
+    public ModelAndView hienThiChucVu() {
+        ModelAndView model = new ModelAndView("index");
+        return model;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Tien Tran
+  Date: 25/09/2018
+  Time: 19:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Trang Chủ</title>
+</head>
+<body>
+    <h1>Kick off dự án 2: Smart Trip</h1>
+</body>
+</html>
