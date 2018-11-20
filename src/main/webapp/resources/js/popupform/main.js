c2a9d690166cb2d18de942cfd80cca6217010c8b
minhddpd01958 <minhddpd01958@fpt.edu.vn>
2018-11-20 22:07:19 +0700

    add login new func, add stastic for admin index 20/11/2018

--- src/main/webapp/resources/js/popupform/main.js
+++ src/main/webapp/resources/js/popupform/main.js
@@ -29,78 +29,78 @@ Author : themelooks.com
 			});
 		}
         
-        var signupForm = $('#signupForm');
-		
-		if ( signupForm.length ) {
-			signupForm.validate({
-				rules: {
-                    singupName: "required",
-                    singupUsername: "required",
-					singupEmail: {
-						required: true,
-						email: true
-					},
-					singupPassword: "required",
-                    singupPasswordAgain: {
-                        equalTo: "#singupPassword"
-                    }
-				},
-				errorPlacement: function (error, element) {
-					return true;
-				}
-			});
-		}
-        
-        var forgotForm = $('#forgotForm');
-		
-		if ( forgotForm.length ) {
-			forgotForm.validate({
-				rules: {
-					forgotEmail: {
-						required: true,
-						email: true
-					}
-				},
-				errorPlacement: function (error, element) {
-					return true;
-				}
-			});
-		}
-        
-        var subscribeForm = $('#subscribeForm');
-		
-		if ( subscribeForm.length ) {
-			subscribeForm.validate({
-				rules: {
-					subscribeEmail: {
-						required: true,
-						email: true
-					}
-				},
-				errorPlacement: function (error, element) {
-					return true;
-				}
-			});
-		}
-        
-        var contactForm = $('#contactForm');
-		
-		if ( contactForm.length ) {
-			contactForm.validate({
-				rules: {
-                    contactName: "required",
-					contactEmail: {
-						required: true,
-						email: true
-					},
-                    contactSubject: "required",
-                    contactMessage: "required"
-				},
-				errorPlacement: function (error, element) {
-					return true;
-				}
-			});
-		}
+        // var signupForm = $('#signupForm');
+		//
+		// if ( signupForm.length ) {
+		// 	signupForm.validate({
+		// 		rules: {
+        //             singupName: "required",
+        //             singupUsername: "required",
+		// 			singupEmail: {
+		// 				required: true,
+		// 				email: true
+		// 			},
+		// 			singupPassword: "required",
+        //             singupPasswordAgain: {
+        //                 equalTo: "#singupPassword"
+        //             }
+		// 		},
+		// 		errorPlacement: function (error, element) {
+		// 			return true;
+		// 		}
+		// 	});
+		// }
+        //
+        // var forgotForm = $('#forgotForm');
+		//
+		// if ( forgotForm.length ) {
+		// 	forgotForm.validate({
+		// 		rules: {
+		// 			forgotEmail: {
+		// 				required: true,
+		// 				email: true
+		// 			}
+		// 		},
+		// 		errorPlacement: function (error, element) {
+		// 			return true;
+		// 		}
+		// 	});
+		// }
+        //
+        // var subscribeForm = $('#subscribeForm');
+		//
+		// if ( subscribeForm.length ) {
+		// 	subscribeForm.validate({
+		// 		rules: {
+		// 			subscribeEmail: {
+		// 				required: true,
+		// 				email: true
+		// 			}
+		// 		},
+		// 		errorPlacement: function (error, element) {
+		// 			return true;
+		// 		}
+		// 	});
+		// }
+        //
+        // var contactForm = $('#contactForm');
+		//
+		// if ( contactForm.length ) {
+		// 	contactForm.validate({
+		// 		rules: {
+        //             contactName: "required",
+		// 			contactEmail: {
+		// 				required: true,
+		// 				email: true
+		// 			},
+        //             contactSubject: "required",
+        //             contactMessage: "required"
+		// 		},
+		// 		errorPlacement: function (error, element) {
+		// 			return true;
+		// 		}
+		// 	});
+		// }
 
 		/* -------------------------------------------------------------------------*
 		 * BACKGROUND IMAGE
